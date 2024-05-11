8efa411ad3007c9ebd1ea5d6413c50725456db0e
Mihail Podzorov <mih.podzorov@gmail.com>
2024-05-11 14:45:55 +0300

    Обновление БД

--- city_of_talents/trash/models.py
+++ city_of_talents/trash/models.py
@@ -6,13 +6,13 @@ from curses.constants import MAX_RANGE_TITLE
 class Trash_File_Link(models.Model):
     title = models.CharField(
         max_length=64,
-        verbose_name='Заголовок'
+        verbose_name='Заголовок',
     )
     file = models.FileField(
         verbose_name='Загрузить файл',
         upload_to='all_file',
         help_text=('Выберите текстовый файл для загрузки на сервер'
-                   'и дальнейшего использования в тексовом редакторе')
+                   'и дальнейшего использования в тексовом редакторе'),
     )
 
     class Meta:
